8076c2c795dfa20d0725a48370ed417428a6a42f
zoolean <147643842+zoolean@users.noreply.github.com>
2024-04-21 01:54:11 +0700

    Update func.py

--- streamlit/func.py
+++ streamlit/func.py
@@ -25,7 +25,39 @@ def create_sum_spend_df(self):
         }, inplace=True)
 
         return sum_spend_df
+
+def create_sum_order_items_df(self):
+        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
+        sum_order_items_df.rename(columns={
+            "product_id": "product_count"
+        }, inplace=True)
+        sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
+
+        return sum_order_items_df
+
+    def review_score_df(self):
+        review_scores = self.df['review_score'].value_counts().sort_values(ascending=False)
+        most_common_score = review_scores.idxmax()
+
+        return review_scores, most_common_score
+
+    def create_bystate_df(self):
+        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
+        bystate_df.rename(columns={
+            "customer_id": "customer_count"
+        }, inplace=True)
+        most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
+        bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
+
+        return bystate_df, most_common_state
+
+    def create_order_status(self):
+        order_status_df = self.df["order_status"].value_counts().sort_values(ascending=False)
+        most_common_status = order_status_df.idxmax()
+
+        return order_status_df, most_common_status
     
+
 class BrazilMapPlotter:
     def __init__(self, data, plt, mpimg, urllib, st):
         self.data = data

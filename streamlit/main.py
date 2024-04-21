0e186afdf9b5616c2eb66f0f4786342df0f1a32f
zoolean <147643842+zoolean@users.noreply.github.com>
2024-04-21 09:00:32 +0700

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -10,89 +10,98 @@ sns.set(style='red')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
-datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
 all_df = pd.read_csv("./dataset/all_data.csv")
-all_df.sort_values(by="order_approved_at", inplace=True)
-all_df.reset_index(inplace=True)
 
 # Geolocation Dataset
 geolocation = pd.read_csv('./dataset/geolocation.csv')
-data = geolocation.drop_duplicates(subset='customer_unique_id')
 
-for col in datetime_cols:
-    all_df[col] = pd.to_datetime(all_df[col])
-
-min_date = all_df["order_approved_at"].min()
-max_date = all_df["order_approved_at"].max()
-
-# Sidebar
-with st.sidebar:
-    # Judul
-    st.title("Welcome to E-Commerce Dashboard")
-
-    # Logo Image
-    st.image("./streamlit/gcl.png")
-
-    # Rentang Tanggal
-    start_date, end_date = st.date_input(
-        label="Pilih Rentang Tanggal",
-        value=[min_date, max_date],
-        min_value=min_date,
-        max_value=max_date
-    )
+# Konversi kolom tanggal ke tipe data datetime
+all_df['order_purchase_timestamp'] = pd.to_datetime(all_df['order_purchase_timestamp'])
 
 
-# Main
-main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
-                 (all_df["order_approved_at"] <= str(end_date))]
+# VISUALISASI 1
+st.title("Distribusi Jumlah Order per Bulan (Jun 2017 - Mei 2018)")
+filtered_orders = all_df[
+    (all_df['order_purchase_timestamp'] >= '2017-06-01') &
+    (all_df['order_purchase_timestamp'] <= '2018-05-31')
+]
+monthly_order_counts = filtered_orders.resample('M', on='order_purchase_timestamp').size()
+fig1, ax1 = plt.subplots(figsize=(12, 6))
+sns.lineplot(x=monthly_order_counts.index.strftime('%B %Y'), y=monthly_order_counts.values, marker='o', color='#72B6A1', ax=ax1)
+ax1.set(xlabel='Bulan', ylabel='Jumlah Order')
+ax1.grid(True, linestyle='--', alpha=0.7)
+st.pyplot(fig1)
 
-function = DataAnalyzer(main_df)
-map_plot = BrazilMapPlotter(data, plt, mpimg, urllib, st)
+# VISUALISASI 2
+st.title("Produk dengan Pembelian Terbanyak dan Terendah")
 
-daily_orders_df = function.create_daily_orders_df()
-sum_spend_df = function.create_sum_spend_df()
-sum_order_items_df = function.create_sum_order_items_df()
-review_score, common_score = function.review_score_df()
-state, most_common_state = function.create_bystate_df()
-order_status, common_status = function.create_order_status()
+# Jumlah semua produk terjual
+purchased_products = all_df['product_category_name_english'].value_counts()
 
-# Title
-st.header("E-Commerce Dashboard :convenience_store:")
+# Produk dengan jumlah pembelian paling banyak
+most_purchased = purchased_products.idxmax()
+most_purchased_count = purchased_products.max()
 
-# Daily Orders
-st.subheader("Daily Orders")
+# Produk dengan jumlah pembelian paling sedikit
+least_purchased = purchased_products.idxmin()
+least_purchased_count = purchased_products.min()
 
-col1, col2 = st.columns(2)
+# Data untuk plot pembelian terbanyak
+top_products = purchased_products.head(6)  # Ambil 6 produk teratas untuk plot
 
-with col1:
-    total_order = daily_orders_df["order_count"].sum()
-    st.markdown(f"Total Order: **{total_order}**")
+# Data untuk plot pembelian tersedikit
+least_purchased_data = all_df[all_df['product_category_name_english'].isin(purchased_products.nsmallest(6).index)]
+least_purchased_data_counts = least_purchased_data['product_category_name_english'].value_counts()
+# Mengurutkan data berdasarkan jumlah pembelian
+least_purchased_data_counts = least_purchased_data_counts.sort_values()
 
-with col2:
-    total_revenue = format_currency(daily_orders_df["revenue"].sum(), "IDR", locale="id_ID")
-    st.markdown(f"Total Revenue: **{total_revenue}**")
+# Set warna palet untuk bar plot
+palette = sns.color_palette("Set2")
 
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.bar(
-    daily_orders_df["order_approved_at"],
-    daily_orders_df["order_count"],
-    color="#ffffff"
-)
-ax.set_xlabel("Date", fontsize=15)
-ax.set_ylabel("Order Count", fontsize=15)
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
+# Warna khusus untuk produk terbanyak dan tersedikit
+special_colors = {'Produk dengan Pembelian Terbanyak': '#72B6A1', 'Produk dengan Pembelian Terendah': '#72B6A1'}
+
+# Mendapatkan posisi produk dengan pembelian tersedikit
+least_purchased_index = least_purchased_data_counts.index.get_loc(least_purchased)
+
+# Plot produk dengan pembelian terbanyak dan terendah secara bersebelahan
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(18, 6))
+
+# Bar plot untuk produk terbanyak
+sns.barplot(x=top_products.index, y=top_products.values, palette=palette, ax=axes[0])
+axes[0].set_title('Produk dengan Pembelian Terbanyak')
+axes[0].set_xlabel('Nama Produk')
+axes[0].set_ylabel('Jumlah Pembelian')
+
+# Menambahkan warna khusus untuk produk terbanyak
+for i, bar in enumerate(axes[0].patches):
+    if i == 0:
+        bar.set_facecolor(special_colors['Produk dengan Pembelian Terbanyak'])
+    else:
+        bar.set_facecolor('#cbd4d1')  # Warna abu-abu
+
+# Bar plot untuk produk tersedikit
+sns.barplot(x=least_purchased_data_counts.index, y=least_purchased_data_counts.values, palette=palette, ax=axes[1])
+axes[1].set_title('Produk dengan Pembelian Terendah')
+axes[1].set_xlabel('Nama Produk')
+axes[1].set_ylabel('Jumlah Pembelian')
+
+# Menambahkan warna khusus untuk produk tersedikit
+for i, bar in enumerate(axes[1].patches):
+    if i == least_purchased_index:
+        bar.set_facecolor(special_colors['Produk dengan Pembelian Terendah'])
+    else:
+        bar.set_facecolor('#cbd4d1')  # Warna abu-abu
+
+plt.tight_layout()
 st.pyplot(fig)
 
-
-
-
-# Customer Demographic
-st.subheader("Customer Demographic")
-tab1 = st.sidebar.radio("Select Tab", ["Geolocation"])
-
-if tab1 == "Geolocation":
-    map_plot.plot()
-
-    with st.expander("See Explanation"):
-        st.write('Berdasarkan visualisasi yang telah disajikan, terdapat sejumlah besar pelanggan yang terletak di wilayah tenggara dan selatan. Selain itu, terlihat bahwa banyak pelanggan berada di kota-kota yang merupakan ibu kota, seperti São Paulo, Rio de Janeiro, Porto Alegre, dan kota-kota lainnya.).')
+# VISUALISASI 3
+st.title("Peta Lokasi Pelanggan")
+brazil_map_image = mpimg.imread(urllib.request.urlopen('https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'), 'jpg')
+fig3, ax3 = plt.subplots(figsize=(10, 10))
+ax3.scatter(geo_df['geolocation_lng'], geo_df['geolocation_lat'], alpha=0.3, s=0.3, c='#72B6A1')
+ax3.set_title('Peta Lokasi Pelanggan')
+ax3.axis('off')
+ax3.imshow(brazil_map_image, extent=[-73.98283055, -33.8, -33.75116944, 5.4])
+st.pyplot(fig3)
